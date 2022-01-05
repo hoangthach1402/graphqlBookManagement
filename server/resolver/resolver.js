3d0e0effc95d3056756178b696e078d684700f3b
hoangthach1402 <vuhoangthach1402@gmail.com>
2022-01-05 10:55:16 +0700

    .

--- server/resolver/resolver.js
+++ server/resolver/resolver.js
@@ -6,6 +6,11 @@ const resolvers = {
     authors: () => authors,
     author: (parent, args) => authors.find((author) => author.id == args.id),
   },
+  Book: {
+    author: (parent, args) => {
+      return authors.find((author) => author.id == parent.authorId)
+    },
+  },
 }
 
 module.exports = resolvers
